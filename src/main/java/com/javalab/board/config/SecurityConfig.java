669b423095cd98cfe7585ee8583ee580b402c0e1
fidnem3 <tjrwo0119@naver.com>
2024-08-13 18:59:58 +0900

    login1

--- src/main/java/com/javalab/board/config/SecurityConfig.java
+++ src/main/java/com/javalab/board/config/SecurityConfig.java
@@ -1,20 +1,16 @@
 package com.javalab.board.config;
 
 import com.javalab.board.handler.AuthFailureHandler;
-import com.javalab.board.handler.AuthSuccessHandler;
 import com.javalab.board.security.CustomOAuth2UserService;
 import com.javalab.board.security.dto.CustomUserDetails;
 import com.javalab.board.security.handler.CustomAccessDeniedHandler;
 import com.javalab.board.security.handler.CustomSocialLoginSuccessHandler;
-import com.javalab.board.service.CompanyService;
 import com.javalab.board.service.CustomUserDetailsService;
-import com.javalab.board.service.JobSeekerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,8 +19,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Configuration
 @EnableWebSecurity
@@ -32,67 +26,47 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 @Slf4j
 public class SecurityConfig {
 
-    @Autowired
-    private  JobSeekerService jobSeekerService;
-
-    @Autowired
-    private  CompanyService companyService;
-
 	@Autowired
 	private  CustomUserDetailsService userDetailsService;
-
-    private final AuthSuccessHandler authSuccessHandler;
 
 	@Autowired
 	private  AuthFailureHandler authFailureHandler;
 
 	@Bean
-	public PasswordEncoder passwordEncoder() {  // 비밀번호 암호화를 위한 빈
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
-	public AccessDeniedHandler accessDeniedHandler() {  // 권한이 없는 페이지에 접근 시 처리를 담당하는 빈
+	public AccessDeniedHandler accessDeniedHandler() {
 		return new CustomAccessDeniedHandler();
 	}
 
 	@Bean
-	public AuthenticationSuccessHandler authenticationSuccessHandler() {  // 소셜 로그인 성공 후처리를 담당하는 빈
+	public AuthenticationSuccessHandler authenticationSuccessHandler() {
 		return new CustomSocialLoginSuccessHandler(passwordEncoder());
 	}
 
-	/**
-	 * [securityFilterChain 메소드]
-	 * - HttpSecurity 빈 등록
-	 * - HttpSecurity 객체를 이용하여 보안 설정
-	 * @param http : HttpSecurity 객체
-	 * @param customOAuth2UserService : CustomOAuth2UserService 객체
-	 * @return SecurityFilterChain : SecurityFilterChain 객체
-	 * @throws Exception : 예외처리
-	 */
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http, CustomOAuth2UserService customOAuth2UserService) throws Exception {
 
-		AuthenticationManagerBuilder auth = http.getSharedObject(AuthenticationManagerBuilder.class);
-
-		auth.userDetailsService(username -> {
-			UserDetails userDetails = jobSeekerService.loadUserByUsername(username);
-			if (userDetails != null) {
-				return userDetails;
-			}
-			userDetails = companyService.loadUserByUsername(username);
-			if (userDetails != null) {
-				return userDetails;
-			}
-			throw new UsernameNotFoundException("User not found with username: " + username);
-		}).passwordEncoder(passwordEncoder());
 		http.userDetailsService(userDetailsService);
 
 		http
 				.formLogin(formLogin -> formLogin
 						.loginPage("/member/login")
-						.loginProcessingUrl("/member/login")
-						.successHandler(authSuccessHandler)
+						.loginProcessingUrl("/member/action")
+						.successHandler((request, response, authentication) -> {
+							CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+							String userType = userDetails.getUserType();
+							if ("company".equals(userType)) {
+								response.sendRedirect("/member/companyPage");
+							} else if ("jobSeeker".equals(userType)) {
+								response.sendRedirect("/member/jobSeekerPage");
+							} else {
+								response.sendRedirect("/");
+							}
+						})
 						.failureHandler(authFailureHandler)
 				)
 				.logout(logout -> logout
@@ -103,17 +77,17 @@ public class SecurityConfig {
 				)
 				.authorizeHttpRequests(authorize -> authorize
 						.requestMatchers("/css/**", "/js/**", "/img/**", "/fonts/**", "/scss/**", "/lib/**", "/assets/**").permitAll()
-						.requestMatchers("/", "/home", "/about", "/contact", "/index", "/jobPost/jobPostList", "/jobPost/jobPostDetail").permitAll()  // 필요에 따라 추가
+						.requestMatchers("/", "/home", "/about", "/contact", "/index", "/jobPost/jobPostList", "/jobPost/jobPostDetail").permitAll()
 						.requestMatchers("/view/**").permitAll()
 						.requestMatchers("/member/**").permitAll()
 						.requestMatchers("/board/**").permitAll()
-						.requestMatchers("/member/adminPage").hasRole("ADMIN")  // 관리자 페이지 접근 권한
-						.requestMatchers("/member/companyPage").hasRole("COMPANY")  // 기업 전용 페이지 접근 권한
-						.requestMatchers("/member/jobSeekerPage").hasRole("USER")  // 구직자 전용 페이지 접근 권한
-						.anyRequest().authenticated() // 모든 요청에 대해 인증 필요
+						.requestMatchers("/member/adminPage").hasRole("ADMIN")
+						.requestMatchers("/member/companyPage").hasRole("COMPANY")
+						.requestMatchers("/member/jobSeekerPage").hasRole("USER")
+						.anyRequest().authenticated()
 				)
 				.sessionManagement(sessionManagement -> sessionManagement
-						.maximumSessions(1) // 한 번에 하나의 세션만 허용
+						.maximumSessions(1)
 						.maxSessionsPreventsLogin(false)
 						.expiredUrl("/login?error=true&exception=Have been attempted to login from a new place. or session expired")
 				)
@@ -128,8 +102,6 @@ public class SecurityConfig {
 						)
 				);
 
-		http.authenticationManager(auth.build());  // 인증 매니저 설정
-
-		return http.build();  // 설정한 HttpSecurity 객체 반환
+		return http.build();
 	}
 }
