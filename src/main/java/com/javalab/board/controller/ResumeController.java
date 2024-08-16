4dcfcd1c4f7a989fc7e5bdeb2193c5cf7afbe779
ESKKorea <wjdrudtns999@naver.com>
2024-08-16 17:13:30 +0900

    오류 수정

--- src/main/java/com/javalab/board/controller/ResumeController.java
+++ src/main/java/com/javalab/board/controller/ResumeController.java
@@ -3,7 +3,6 @@ package com.javalab.board.controller;
 
 import com.javalab.board.dto.BoardDto;
 import com.javalab.board.dto.ResumeDto;
-import com.javalab.board.dto.ResumeFileDto;
 import com.javalab.board.dto.ResumeSkillDto;
 import com.javalab.board.service.JobSeekerService;
 import com.javalab.board.service.ResumeService;
@@ -63,7 +62,7 @@ public class ResumeController {
     //  저장 폼 전송
 
     @PostMapping("/save")
-    public String save(@ModelAttribute("resumeDto") ResumeDto resumeDto) throws IOException {
+    public String save(@ModelAttribute("resumeDto") ResumeDto resumeDto) {
         System.out.println("resumeDto = " + resumeDto);
         resumeService.resumeCreate(resumeDto);
         return "index";
@@ -90,11 +89,7 @@ public class ResumeController {
         ResumeDto resumeDto = resumeService.findById(resumeId);
 
         model.addAttribute("resume", resumeDto);
-        System.out.println("resumeDto =" + resumeDto);
-        if (resumeDto.getFileAttached() == 1 ) {
-            ResumeFileDto resumeFileDto = resumeService.findFile(resumeId);
-            model.addAttribute("resumeFile" , resumeFileDto);
-        }
+
         return "resume/detail";
     }
 
--- src/main/java/com/javalab/board/service/ResumeService.java
+++ src/main/java/com/javalab/board/service/ResumeService.java
@@ -1,16 +1,15 @@
 package com.javalab.board.service;
 
 import com.javalab.board.dto.ResumeDto;
-import com.javalab.board.dto.ResumeFileDto;
+import com.javalab.board.dto.ResumeSkillDto;
 import com.javalab.board.repository.ResumeMapper;
 import com.javalab.board.repository.ResumeSkillMapper;
+import com.javalab.board.vo.BoardVo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.awt.*;
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -22,83 +21,43 @@ public class ResumeService {
     private final ResumeSkillMapper resumeSkillMapper;
 
 
-    public void resumeCreate(ResumeDto resumeDto) throws IOException {
-        if (resumeDto.getResumeFile().isEmpty()) {
-            // 파일 없다.
-            resumeDto.setFileAttached(0);
-            resumeMapper.resumeCreate(resumeDto);
-        } else {
-            // 파일 있다.
-            resumeDto.setFileAttached(1);
-            // 게시글 저장 후 id값 활용을 위해 리턴 받음.
-            ResumeDto savedResume = resumeMapper.resumeCreate(resumeDto);
-            // 파일만 따로 가져오기
-            MultipartFile boardFile = resumeDto.getResumeFile();
-            // 파일 이름 가져오기
-            String originalFilename = boardFile.getOriginalFilename();
-            System.out.println("originalFilename = " + originalFilename);
-            // 저장용 이름 만들기
-            System.out.println(System.currentTimeMillis());
-            String storedFileName = System.currentTimeMillis() + "-" + originalFilename;
-            System.out.println("storedFileName = " + storedFileName);
-            // BoardFileDTO 세팅
-            ResumeFileDto resumeFileDto = new ResumeFileDto();
-            resumeFileDto.setOriginalFileName(originalFilename);
-            resumeFileDto.setStoredFileName(storedFileName);
-            resumeFileDto.setResumeId(savedResume.getResumeId());
-            // 파일 저장용 폴더에 파일 저장 처리
-//                String savePath = "/Users/codingrecipe/development/intellij_community/spring_upload_files/" + storedFileName; // mac
-            String savePath = "C:/upload/" + storedFileName;
-            boardFile.transferTo(new File(savePath));
-            // board_file_table 저장 처리
-            resumeMapper.saveFile(resumeFileDto);
+    @Transactional
+    public void resumeCreate(ResumeDto resumeDto) {
+        // 1. 이력서를 저장합니다.
+        resumeMapper.createResume(resumeDto);
+
+        // 2. 기술 리스트를 문자열로 변환하여 저장합니다.
+        String skillsAsString = resumeDto.getSkillsAsString();
+        if (!skillsAsString.isEmpty()) {
+            ResumeSkillDto resumeSkillDto = new ResumeSkillDto();
+            resumeSkillDto.setResumeId(resumeDto.getResumeId());
+            resumeSkillDto.setSkill(skillsAsString); // 변환된 문자열을 저장
+            resumeSkillMapper.resumeSkillCreate(resumeSkillDto);
         }
     }
 
+    public List<ResumeDto> findAll() {
+        return resumeMapper.findAll();
 
-
-//    @Transactional
-//    public void resumeCreate(ResumeDto resumeDto) {
-//        // 1. 이력서를 저장합니다.
-//        resumeMapper.resumeCreate(resumeDto);
-//
-//        // 2. 기술 리스트를 문자열로 변환하여 저장합니다.
-//        String skillsAsString = resumeDto.getSkillsAsString();
-//        if (!skillsAsString.isEmpty()) {
-//            ResumeSkillDto resumeSkillDto = new ResumeSkillDto();
-//            resumeSkillDto.setResumeId(resumeDto.getResumeId());
-//            resumeSkillDto.setSkill(skillsAsString); // 변환된 문자열을 저장
-//            resumeSkillMapper.resumeSkillCreate(resumeSkillDto);
-//        }
-//
-//
-//    }
-
-public List<ResumeDto> findAll() {
-    return resumeMapper.findAll();
-
-}
+    }
 
 //    public void updateHits(int resumeId) {
 //        resumeMapper.updateHits(resumeId);
 //    }
 
-public ResumeDto findById(int resumeId) {
-    return resumeMapper.findById(resumeId);
-}
-
-
-public void updateResume(ResumeDto resumeDto) {
-    resumeMapper.updateResume(resumeDto);
-}
-
-public void deleteResume(int resumeId) {
-    resumeMapper.deleteResume(resumeId);
-}
-
-    public ResumeFileDto findFile(int resumeId) {
-        return resumeMapper.findFile(resumeId);
+    public ResumeDto findById(int resumeId) {
+        return resumeMapper.findById(resumeId);
     }
+
+
+    public void updateResume(ResumeDto resumeDto) {
+        resumeMapper.updateResume(resumeDto);
+    }
+
+    public void deleteResume(int resumeId) {
+        resumeMapper.deleteResume(resumeId);
+    }
+
 }
 
 
