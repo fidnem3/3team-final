bc5cac510682e792a88e090fcf6705eb270a0418
kyni0124@naver.com <kyni0124@naver.com>
2024-08-20 15:56:09 +0900

    블랙 디비 및 목록/삭제

--- src/main/java/com/javalab/board/dto/BlacklistDto.java
+++ src/main/java/com/javalab/board/dto/BlacklistDto.java
@@ -15,7 +15,34 @@ public class BlacklistDto {
     private Long blackId;
     private String compId;
     private String reason;
-    private Date blacklistDate;
+    private Date blacklistDate;  // 기존 Date 타입 유지
     private String type;
-    private boolean isBlacklisted;
+
+    @Builder.Default
+    private Integer isBlacklisted = 0;  // boolean 대신 Integer 사용
+
+    // 편의 메소드 추가
+    public boolean isBlacklisted() {
+        return isBlacklisted != null && isBlacklisted == 1;
+    }
+
+    public void setBlacklisted(boolean blacklisted) {
+        this.isBlacklisted = blacklisted ? 1 : 0;
+    }
+
+    // type 필드에 대한 유효성 검사
+    public void setType(String type) {
+        if (!"jobSeeker".equals(type) && !"company".equals(type)) {
+            throw new IllegalArgumentException("Type must be either 'jobSeeker' or 'company'");
+        }
+        this.type = type;
+    }
+
+    // isBlacklisted 필드에 대한 유효성 검사
+    public void setIsBlacklisted(Integer isBlacklisted) {
+        if (isBlacklisted != null && (isBlacklisted != 0 && isBlacklisted != 1)) {
+            throw new IllegalArgumentException("isBlacklisted must be either 0 or 1");
+        }
+        this.isBlacklisted = isBlacklisted;
+    }
 }
