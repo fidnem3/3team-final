1b0394aa5a5c8c3ffe759066bb0189c748712f85
loismeow <sb02085@gmail.com>
2024-08-23 17:23:27 +0900

    test

---
+++ src/main/java/com/javalab/board/repository/RequiredSkillMapper.java
@@ -0,0 +1,16 @@
+package com.javalab.board.repository;
+
+import com.javalab.board.dto.RequiredSkillDto;
+import com.javalab.board.dto.ResumeSkillDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface RequiredSkillMapper {
+
+     public Long requiredSkillCreate(RequiredSkillDto requiredSkillDto);
+
+    List<String> getRequiredSkillsByJobPostId(Long jobPostId);
+
+}
